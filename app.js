4f83914696ede08cee5d704735aff2b6c4d2ce77
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-01-16 14:35:53 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+
+  if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<html>`);
+    res.write(`<head><title>My First Page</title><head>`);
+    res.write(`<body><h1>Welcome Home</h1></body>`);
+    res.write(`</html>`);
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<html>`);
+    res.write(`<head><title>My First Page</title><head>`);
+    res.write(`<body><h1>Welcome to About US</h1></body>`);
+    res.write(`</html>`);
+  } else if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<html>`);
+    res.write(`<head><title>My First Page</title><head>`);
+    res.write(`<body><h1>Hello From NodeJS Server!</h1></body>`);
+    res.write(`</html>`);
+  }
 });
 
 server.listen(4000, () => {
