f94016bbc524a4e6139f4a102ecb91f6ff3405ea
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-01-17 13:11:18 +0530

    Body Parser added

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`In the MiddleWare`);
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST"><p>Product:</p><input type="text" name="title"><p>Size:</p><input type="number" name="size"><button type="submit">Add Product</button></form>`
+  );
 });
 
-app.use((req, res, next) => {
-  console.log(`In another middleware`);
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send(`<h2>Hello From Express!</h2>`);
 });
 
